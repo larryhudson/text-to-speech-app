20182b5fb9073e927c6aaa308f9f2e4525be5548
Larry Hudson <larryhudson@hey.com>
2021-12-11 18:53:45 +1100

    regenerate secret key and hide it

--- text_to_audiobook_app/text_to_audiobook_app/settings.py
+++ text_to_audiobook_app/text_to_audiobook_app/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%sxrnpvgw#8l@nct8hy8if8771mqw82g+v7lxpk1tk^aqe12u)'
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
